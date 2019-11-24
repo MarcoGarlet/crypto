6e474f1c1e444cbd8cfdeca5cbf44db5bd927bfa
Marco Garlet <marcogarlet@gmail.com>
2019-11-24 16:09:45 +0100

    AES implementation 10 round

--- aes128/aes128.py
+++ aes128/aes128.py
@@ -63,8 +63,7 @@ def gf_invert(a, mod=0x1B) :
 rshift = lambda x,y: [x[(i-y*1)%8] for i in range(8)]
 int2vect = lambda c: [int(bit) for bit in bin(c%16).split('b')[1][::-1]+('0'*(4-len(bin(c%16).split('b')[1])))+bin(c//16%16).split('b')[1][::-1]+('0'*(4-len(bin(c//16%16).split('b')[1])))] 
 vect2int = lambda v: int(''.join([str(i) for i in v])[::-1],2)
- 
-
+transposed_matrix = lambda v: [[v[i][i1] for i in range(len(v))] for i1 in range(len(v[0]))] 
 cvector = int2vect(0x63)
 cmatrix = [[1,0,0,0,1,1,1,1]]+[rshift([1,0,0,0,1,1,1,1],i) for i in range(1,8)]
 
@@ -80,16 +79,16 @@ def decimal2bin(n,t):
   while len(b)<t: b+=[0]
   return b[::-1]
 
-passwd = input('Insert secret key [len <= 16]: ')
+#passwd = input('Insert secret key [len <= 16]: ')
 
-#passwd='\x0f\x15\x71\xc9\x47\xd9\xe8\x59\x0c\xb7\xad\xd6\xaf\x7f\x67\x98'
+passwd='\x0f\x15\x71\xc9\x47\xd9\xe8\x59\x0c\xb7\xad\xd6\xaf\x7f\x67\x98'
 
 while len(passwd)>16: passwd = input('Insert secret key [len <= 16]: ')
 while len(passwd)<16: passwd+='\x00'
 
 key = to4matrix(passwd)
 
-whitening = lambda key,plaintext: [[ key[i1][i] ^ plaintext[i1][i] for i1 in range(4)] for i in range(4)]
+whitening = lambda key,plaintext: [[ key[i1][i] ^ plaintext[i1][i] for i in range(4)] for i1 in range(4)]
 
 rotword = lambda x: [x[1],x[2],x[-1],x[0]]
 subword = lambda x: sbox_v(x)
@@ -100,61 +99,105 @@ def expand_key(key):
   w = [key]
   rcon = 0x1
   for i in range(10):
-    print('STAGE {}'.format(i))
+    #print('STAGE {}'.format(i))
     current_w=w[-1]
     wl = current_w[-1]
-    #ROTWORD
     rcon_v = [rcon,0,0,0]
     rcon=multGF2(rcon,2)
     
     x=rotword(wl)
-    print([hex(el) for el in rcon_v])
+    #print([hex(el) for el in rcon_v])
     y=[subword(el) for el in x]
     z=[rcon_v[i]^y[i] for i in range(4)]
-    print([hex(el) for el in z])
+    #print([hex(el) for el in z])
     wn = [current_w[0][i] ^ z[i] for i in range(4)]
-    print([hex(el) for el in wn])
+    #print([hex(el) for el in wn])
     wn1= [wn[i] ^ current_w[1][i] for i in range(4)]
     wn2= [wn1[i] ^ current_w[2][i] for i in range(4)]
     wn3= [wn2[i] ^ current_w[3][i] for i in range(4)]
     w+=[[wn,wn1,wn2,wn3]]
     
     #RCON
-  for m in w:
-    for l in m:
-      for el in l:
-        print(hex(el),end=' ')
-      print()
-    print('--------------------------')
-    
+  return w   
 
-expand_key(key)
+round_keys = expand_key(key)
 
-def add_round_key():
-  pass
+def add_round_key(r,st_round):
+  roundk = transposed_matrix(round_keys[r]) 
+  return [[st_round[i][i1]^roundk[i][i1] for i1 in range(4)] for i in range(4)] 
 
-def sub_bytes():
-  pass
+def sub_bytes(st_round):
+  return [[sbox_v(el) for el in st_roundr] for st_roundr in st_round]
 
-def shift_rows():
-  pass
+def shift_rows(st_round):
+  return [[st_round[i][(i1+i)%4] for i1 in range(4)] for i in range(4)]
 
-def mix_columns():
-  pass
+def mix_columns(st_round):
+  matrixc = [[2,3,1,1],[1,2,3,1],[1,1,2,3],[3,1,1,2]]
+  cvectors = [[st_round[i1][i] for i1 in range(4)] for i in range(4)]
+  st_roundn = [[list(map(lambda x,y: multGF2(x,y),mrows,cvector)) for mrows in matrixc] for cvector in cvectors]
+  return [[reduce(lambda x,y:x^y, st_roundn[i][j]) for i in range(4)] for j in range(4)]
 
-
-
-def round(pl):
-  pass
-
+def print_matrix(m):
+  print()
+  for r in m:
+    for el in r:
+      print(hex(el), end=' ')
+    print()
+  print()
 
 if __name__=='__main__':
-  plaintext = input('insert plaintext (16 char): ')
+  #plaintext = input('insert plaintext (16 char): ')
+  plaintext = '\x01\x89\xfe\x76\x23\xab\xdc\x54\x45\xcd\xba\x32\x67\xef\x98\x10'
   while len(plaintext) != 16: plaintext = input('insert plaintext (16 char): ')
   plaintext = to4matrix(plaintext)
-  plaintext_w = whitening(plaintext, key)
+  print('\nPLAINTEXT')
+  print_matrix(plaintext)
+  print('SECRET KEY')
+  print_matrix(transposed_matrix(round_keys[0]))
+  st_round = whitening(plaintext, transposed_matrix(round_keys[0]))
+  
+  print('PLAINTEXT AFTER WHITENING')
+  print_matrix(st_round)
+  
   for round in range(10):
-    #
-    print(round)
+    if round != 9:
+      print('--------------------------------------------')
+      print('round = {}\n\n'.format(round+1))
+      print('START OF ROUND')
+      print_matrix(st_round)
+      st_round = sub_bytes(st_round)
+      print('AFTER SUB BYTES')
+      print_matrix(st_round)
+      st_round=shift_rows(st_round)
+      print('AFTER SHIFT ROWS')
+      print_matrix(st_round)
+      st_round=mix_columns(st_round)
+      print('AFTER MIX COLUMNS')
+      print_matrix(st_round)
+      st_round=add_round_key(round+1, st_round)
+      print('AFTER ADD ROUND KEY')
+      print_matrix(st_round)
+    else:
+      print('--------------------------------------------')
+      print('round = LAST\n\n')
+      print('START OF ROUND')
+      print_matrix(st_round)
+      st_round = sub_bytes(st_round)
+      print('AFTER SUB BYTES')
+      print_matrix(st_round)
+      st_round=shift_rows(st_round)
+      print('AFTER SHIFT ROWS')
+      print_matrix(st_round)
+      st_round=add_round_key(round+1, st_round)
+      print('AFTER ADD ROUND KEY')
+      print_matrix(st_round)
+      print('--------------------------------------------')
+   
+
+
+
+
+
     
 
